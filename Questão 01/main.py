a084b7a9791dfdd4cedab2e36bfd44f7408dbca4
Pedro Lucas <plrf100@gmail.com>
2022-04-21 22:57:20 -0300

    fix(code): removin unnecessary code

--- Questão 01/main.py
+++ Questão 01/main.py
@@ -7,11 +7,6 @@ a = np.array(im)
 
 
 def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
-    """
-    Converts an RGB image to YIQ color space
-    :param imgRGB: An Image in RGB
-    :return: A YIQ in image color space
-    """
     yiq_from_rgb = np.array([[0.299, 0.587, 0.114],
                              [0.59590059, -0.27455667, -0.32134392],
                              [0.21153661, -0.52273617, 0.31119955]])
@@ -22,11 +17,6 @@ def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
 
 
 def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
-    """
-    Converts an YIQ image to RGB color space
-    :param imgYIQ: An Image in YIQ
-    :return: A RGB in image color space
-    """
     yiq_from_rgb = np.array([[0.299, 0.587, 0.114],
                              [0.59590059, -0.27455667, -0.32134392],
                              [0.21153661, -0.52273617, 0.31119955]])
@@ -37,7 +27,5 @@ def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
 
 test = transformRGB2YIQ(a)
 test2 = Image.fromarray(transformYIQ2RGB(test).astype(np.uint8))
-# print(transformYIQ2RGB(test).astype(int))
-
 
 test2.save("Woman_eye_yiq.png")
