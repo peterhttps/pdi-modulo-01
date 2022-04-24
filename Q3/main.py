612a17d23ff872384803cba1be4c969c2f9fc209
Pedro Gadelha <ph-cg@hotmail.com>
2022-04-24 01:17:19 -0300

    modificacoes sobel

--- Q3/main.py
+++ Q3/main.py
@@ -72,21 +72,25 @@ def sobel(img):
 
     for i in range(i, h-1, 1):
         for j in range(j, w-1, 1):
-
-            count_x = 0
+            #count_x = 0
             for k in range(k, m+1, 1):
-                count_y = 0
+                #count_y = 0
                 for l in range(l, n+1, 1):
-                    if (k+i < 0 or k+i > h-1) or (l+j < 0 or l+j > w-1):
-                        dfi += 0
-                        dfj += 0
-                    else:
-                       #print(count_x, count_y)
-                       dfi = np.add(dfi, np.multiply(vertical[count_x, count_y], img[k+i, l+j]))
-                       dfj = np.add(dfj, np.multiply(horizontal[count_x, count_y], img[k+i, l+j]))
-                    count_y += 1
+                    count_x = 0
+                    for count_x in range(count_x, 3, 1):
+                        count_y = 0
+                        for count_y in range(count_y, 3, 1):
+                            if (k+i < 0 or k+i > h-1) or (l+j < 0 or l+j > w-1):
+                                dfi += np.add(dfi, 0)
+                                dfj += np.add(dfj, 0)
+                            else:
+                            #print(count_x, count_y)
+                            #print(vertical[count_x, count_y], img[k+i, l+j], vertical[count_x, count_y]*img[k+i, l+j])
+                                dfi = np.add(dfi, np.multiply(vertical[count_x, count_y], img[k+i, l+j]))
+                                dfj = np.add(dfj, np.multiply(horizontal[count_x, count_y], img[k+i, l+j]))
+                    #count_y += 1
                 l = -n
-                count_x += 1
+                #count_x += 1
             k = -m
             soma = np.add(abs(dfi), abs(dfj))
             resultado[i, j] = soma
@@ -107,6 +111,8 @@ def sobel(img):
 def histograma(sob, img_shape):
     h, w, c = img_shape
     i = j = 0
+    maior = np.amax(sob)
+    menor = np.amin(sob)
     resultado = np.zeros(img_shape, dtype='uint8') 
 
     #print(np.amax(sob))
@@ -114,20 +120,24 @@ def histograma(sob, img_shape):
 
     for i in range(i, h-1, 1):
         for j in range(j, w-1, 1):
-            tr = np.round_(((sob[i, j] - np.amin(sob))/(np.amax(sob) - np.amin(sob)))*((np.amax(sob) - 1) - 1)) 
+            tr = np.round_(np.multiply(np.divide(np.subtract(sob[i, j, 0], menor), maior - menor), 254))
+            #tr = round(((sob[i, j] - menor)/(maior - menor))*(254))
+            #tr_G = np.round_(((sob[i, j, 1] - menor)/(maior - menor))*(maior - 1))
+            #tr_B = np.round_(((sob[i, j, 2] - menor)/(maior - menor))*(maior - 1))
+            #print(sob[i, j], menor, maior, tr)
             resultado[i, j] = tr
         j = 0
     #print(resultado)
 
     return resultado
 
-img_resultante_mediana = mediana(a)
+#img_resultante_mediana = mediana(a)
 img_resultante_sobel = sobel(a)
 
-img_resultante_rgb = Image.fromarray(img_resultante_mediana.astype(np.uint8))
+#img_resultante_rgb = Image.fromarray(img_resultante_mediana.astype(np.uint8))
 img_resultante_rgb2 = Image.fromarray(img_resultante_sobel.astype(np.uint8))
 
-img_resultante_rgb.save("medianaRGB.png")
+#img_resultante_rgb.save("medianaRGB.png")
 img_resultante_rgb2.save("sobel.png")
 
 
